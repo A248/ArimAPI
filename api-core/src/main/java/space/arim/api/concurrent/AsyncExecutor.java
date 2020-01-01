62d84e3b137057e6060ea0ceebe160fa8aad665c
A248 <anandebeh@gmail.com>
2020-01-01 12:02:34 -0500

    Remove ArimAPI-framework, consolidate into ArimAPI-core

--- api-core/src/main/java/space/arim/api/concurrent/AsyncExecutor.java
+++ api-core/src/main/java/space/arim/api/concurrent/AsyncExecutor.java
@@ -16,7 +16,7 @@
  * along with ArimAPI. If not, see <https://www.gnu.org/licenses/>
  * and navigate to version 3 of the GNU General Public License.
  */
-package space.arim.api.framework;
+package space.arim.api.concurrent;
 
 import space.arim.universal.registry.Registrable;
 
