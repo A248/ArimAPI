a9d405ace44f697c0e5c91a7748b0be4b6420d79
A248 <anandebeh@gmail.com>
2020-01-08 19:43:25 -0500

    Add SimpleExecutor implementation of BasicExecutor

---
+++ api-core/src/main/java/space/arim/api/concurrent/SimpleExecutor.java
@@ -0,0 +1,48 @@
+/* 
+ * ArimAPI, a minecraft plugin library and framework.
+ * Copyright © 2020 Anand Beh <https://www.arim.space>
+ * 
+ * ArimAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ArimAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ArimAPI. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU General Public License.
+ */
+package space.arim.api.concurrent;
+
+import java.util.concurrent.Executor;
+
+/**
+ * 
+ * A simple implementation of {@link BasicExecutor}
+ * 
+ * @author A248
+ *
+ */
+public class SimpleExecutor implements BasicExecutor {
+
+	private Executor executor;
+	
+	/**
+	 * Creates a SimpleExecutor from an existing executor. Essentially, using a SimpleExecutor in this manner adds functionality at no additional cost.
+	 * 
+	 * @param executor the underlying {@link Executor}
+	 */
+	public SimpleExecutor(Executor executor) {
+		this.executor = executor;
+	}
+	
+	@Override
+	public void execute(Runnable command) {
+		executor.execute(command);
+	}
+	
+}
