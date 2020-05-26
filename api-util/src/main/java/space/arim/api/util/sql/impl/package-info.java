b7d7c0a65dc5d1394df5420bdcdde388be9f42c8
A248 <anandebeh@gmail.com>
2020-05-26 10:03:16 -0400

    Fix typos in documentation of space.arim.api.util.sql.impl

--- api-util/src/main/java/space/arim/api/util/sql/impl/package-info.java
+++ api-util/src/main/java/space/arim/api/util/sql/impl/package-info.java
@@ -1,8 +1,8 @@
 /**
- * Classes in this package are used to help implement implement the SQL framework {@link space.arim.api.util.sql}.
+ * Classes in this package are used to help implement the SQL framework {@link space.arim.api.util.sql}.
  * They mostly consist of simple, boilerplate implementations for closable interfaces. <br>
  * <br>
- * The general form of the name of many implementation classes is one which begins with the implemented interface's name,
+ * The general form of the name of classes in this package is one which begins with the implemented interface's name,
  * and ends with "WithPreparedStatement", "WithPreparedStatementAndConnection", or "WithConnection". In any such case,
  * the accompanying objects (of the types described after the "With", such as PreparedStatement) will be closed when
  * the overall interface is closed. These accompanying objects must be passed to the constructor of the implementing class. <br>
