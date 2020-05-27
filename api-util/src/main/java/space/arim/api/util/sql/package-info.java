c60c1a6f2e76d002f660d9bba578612b53cac4ae
A248 <anandebeh@gmail.com>
2020-05-27 13:40:24 -0400

    Improve documentation of space.arim.api.util.sql

--- api-util/src/main/java/space/arim/api/util/sql/package-info.java
+++ api-util/src/main/java/space/arim/api/util/sql/package-info.java
@@ -2,8 +2,8 @@
  * A framework for SQL usage aiming at simplifying queries and cleaner code. <br>
  * <br>
  * Within all classes in this package, no try/catch statements are ever made. All exceptions are relayed to the caller. <br>
- * <br>
- * The primary interface is {@link SqlBackend}.
+ * It is intended that the caller may execute SQL queries, using the primary interface {@link SqlBackend}, with
+ * a single try-with-resources block.
  * 
  */
 package space.arim.api.util.sql;
