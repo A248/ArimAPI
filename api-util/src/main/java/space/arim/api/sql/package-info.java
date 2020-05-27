8f75290b36c5ca0d8cd342a921ce008f5bae8094
A248 <anandebeh@gmail.com>
2020-05-27 10:30:14 -0400

    Better generalisation of deprecation of space.arim.api.sql

--- api-util/src/main/java/space/arim/api/sql/package-info.java
+++ api-util/src/main/java/space/arim/api/sql/package-info.java
@@ -2,5 +2,9 @@
  * A basic framework for SQL usage. <b>This entire framework is now deprecated.
  * Use {@link space.arim.api.util.sql} instead.</b>
  * 
+ * @deprecated The SQL framework in this package is poorly designed. It contains
+ * assumed contracts and implicit requirements which only one who has implemented
+ * the framework would understand. Some of the major components of this framework
+ * have more specific reasons for their deprecation.
  */
 package space.arim.api.sql;
