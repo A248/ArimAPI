940a0be6c493e282416059110bf9466c8c185baa
A248 <anandebeh@gmail.com>
2020-06-09 08:46:05 -0400

    Add space.arim.api.util.config package-info

---
+++ api-util/src/main/java/space/arim/api/util/config/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A multi-platform, much-improved configuration utility whose primary interface is {@link Config}. <br>
+ * <br>
+ * {@link SimpleConfig} is a common-sense implementation.
+ * 
+ */
+package space.arim.api.util.config;
