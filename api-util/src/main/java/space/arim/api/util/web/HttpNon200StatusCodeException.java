2b7f574f77a1eff37c309ae22b89ea418762d5c6
A248 <anandebeh@gmail.com>
2020-06-12 21:30:51 -0400

    Document HttpNon200StatusCodeException

--- api-util/src/main/java/space/arim/api/util/web/HttpNon200StatusCodeException.java
+++ api-util/src/main/java/space/arim/api/util/web/HttpNon200StatusCodeException.java
@@ -32,18 +32,38 @@ public class HttpNon200StatusCodeException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 2041055985968858366L;
 
+	/**
+	 * Creates an exception
+	 * 
+	 */
 	public HttpNon200StatusCodeException() {
 		
 	}
 	
+	/**
+	 * Creates the exception with the specified HTTP status code
+	 * 
+	 * @param code the status code
+	 */
 	public HttpNon200StatusCodeException(int code) {
 		super("Status code " + code);
 	}
 	
+	/**
+	 * Creates the exception with the specified cause
+	 * 
+	 * @param cause the cause
+	 */
 	public HttpNon200StatusCodeException(Throwable cause) {
 		super(cause);
 	}
 	
+	/**
+	 * Crates the exception with the specified HTTP status code and cause
+	 * 
+	 * @param code the status code
+	 * @param cause the cause
+	 */
 	public HttpNon200StatusCodeException(int code, Throwable cause) {
 		super("Status code " + code, cause);
 	}
