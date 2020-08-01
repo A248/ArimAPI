45c05ec88b0173efc4c6779170e2cb105fb1caaa
A248 <anandebeh@gmail.com>
2020-08-01 14:19:36 -0400

    Include and recommend arimapi-configure

--- api-util/src/main/java/space/arim/api/util/config/package-info.java
+++ api-util/src/main/java/space/arim/api/util/config/package-info.java
@@ -1,7 +1,11 @@
 /**
- * A multi-platform, much-improved configuration utility whose primary interface is {@link Config}. <br>
+ * A multi-platform configuration utility whose primary interface is {@link Config}. <br>
  * This config framework will only throw exceptions where documented, and most of such are subclasses of
- * {@link ConfigException}.
+ * {@link ConfigException}. <br>
+ * <br>
+ * <b>Users are generally encouraged to migrate to <i>arimapi-configure</i>, which is far more comprehensive</b>.
+ * The classes in this package are in maintenance mode. See {@code space.arim.api.configure} for more details
+ * on the recommended alternative.
  * 
  */
 package space.arim.api.util.config;
