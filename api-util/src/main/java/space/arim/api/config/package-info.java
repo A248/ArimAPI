b83d23983af1a88d21fccba1b1fbd96531a99cca
A248 <anandebeh@gmail.com>
2020-08-01 14:12:17 -0400

    Remove old config classes (space.arim.api.config)

--- api-util/src/main/java/space/arim/api/config/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * A configuration utility.
- * 
- * @deprecated This config framework assumes the designer wishes for specific behaviour. It is inflexible,
- * and thus, as a framework, hard to change when other behaviour is desired. {@link space.arim.api.util.config}
- * should be used instead.
- */
-package space.arim.api.config;
