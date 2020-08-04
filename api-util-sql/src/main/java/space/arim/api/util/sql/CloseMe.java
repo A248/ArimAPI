f3a267b29e9f1ef439dc80b6c5c990e8bf871211
A248 <anandebeh@gmail.com>
2020-08-04 19:34:44 -0400

    Update license headers for new util-sql and util-web modules

--- api-util-sql/src/main/java/space/arim/api/util/sql/CloseMe.java
+++ api-util-sql/src/main/java/space/arim/api/util/sql/CloseMe.java
@@ -1,19 +1,19 @@
 /* 
- * ArimAPI-util
+ * ArimAPI-util-sql
  * Copyright © 2020 Anand Beh <https://www.arim.space>
  * 
- * ArimAPI-util is free software: you can redistribute it and/or modify
+ * ArimAPI-util-sql is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * ArimAPI-util is distributed in the hope that it will be useful,
+ * ArimAPI-util-sql is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with ArimAPI-util. If not, see <https://www.gnu.org/licenses/>
+ * along with ArimAPI-util-sql. If not, see <https://www.gnu.org/licenses/>
  * and navigate to version 3 of the GNU General Public License.
  */
 package space.arim.api.util.sql;
