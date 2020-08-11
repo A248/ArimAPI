286f27cc0d2fc2dd11cb65e9bf9f9e4efcdbfb9e
A248 <anandebeh@gmail.com>
2020-08-11 10:06:21 -0400

    Put space.arim.api.util.sql in maintenance mode

--- api-util-sql/src/main/java/space/arim/api/util/sql/package-info.java
+++ api-util-sql/src/main/java/space/arim/api/util/sql/package-info.java
@@ -1,12 +1,12 @@
 /**
  * A framework for SQL usage aiming at simplifying queries and cleaner code. <br>
  * <br>
- * Within all classes in this package, no try/catch statements are ever made. All exceptions are relayed to the caller. <br>
- * It is intended that the caller may execute SQL queries, using the primary interface {@link SqlBackend}, with
- * a single try-with-resources block. <br>
+ * <b>This framework is now in maintenance mode. The preferred alternative is
+ * <a href="https://github.com/A248/JdbCaesar">JdbCaesar</a></b> <br>
  * <br>
- * Since 0.13.0, every SQL-related method in this framework throws {@link java.sql.SQLException SQLException}, so that
- * implementations have maximum flexibility in throwing such exceptions.
+ * Within all classes in this package, few try/catch statements are ever made. It is intended that the caller
+ * may execute SQL queries, using the primary interface {@link SqlBackend}, with a single try-with-resources block.
+ * Every SQL-related method in this framework throws {@link java.sql.SQLException SQLException}, so that implementations have maximum flexibility in throwing such exceptions.
  * 
  */
 package space.arim.api.util.sql;
