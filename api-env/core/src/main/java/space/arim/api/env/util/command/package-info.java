98d4a90ce3ff8c23b9b41c6de83b3597d74366ca
A248 <anandebeh@gmail.com>
2020-08-30 17:34:07 -0400

    Add BukkitCommandSkeleton/SpongeCommandSkeleton

---
+++ api-env/core/src/main/java/space/arim/api/env/util/command/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Utility package providing straightforward command base implementations for platforms,
+ * omitting the platform specific usage/description/permission malarkey.
+ * 
+ */
+package space.arim.api.env.util.command;
