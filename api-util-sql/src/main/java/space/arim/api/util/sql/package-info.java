f4a4abe11c8f0c8393c456ced156bc6226e88726
A248 <anandebeh@gmail.com>
2020-10-22 12:54:52 -0400

    Change replacement recommendation on space.arim.api.util.sql

--- api-util-sql/src/main/java/space/arim/api/util/sql/package-info.java
+++ api-util-sql/src/main/java/space/arim/api/util/sql/package-info.java
@@ -5,9 +5,7 @@
  * may execute SQL queries, using the primary interface {@link SqlBackend}, with a single try-with-resources block.
  * Every SQL-related method in this framework throws {@link java.sql.SQLException SQLException}, so that implementations have maximum flexibility in throwing such exceptions.
  * 
- * @deprecated This framework is deprecated for removal. The recommended alternative is the
- * <a href="https://github.com/A248/JdbCaesar">JdbCaesar</a> library by the same author of this framework,
- * which has significant design improvements over this one. JdbCaesar is more powerful, more resilient,
- * and better structured.
+ * @deprecated This framework is deprecated for removal. The recommended alternative is either the
+ * JOOQ or JDBI libraries.
  */
 package space.arim.api.util.sql;
