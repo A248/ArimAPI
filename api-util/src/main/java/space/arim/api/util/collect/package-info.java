a1388fb76046e23a5e24d771255dec86daa000e6
A248 <anandebeh@gmail.com>
2020-12-03 12:52:54 -0500

    Remove api-util, whose contents were deprecated for removal
    No replacement

--- api-util/src/main/java/space/arim/api/util/collect/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains implementations of dynamically calculated maps.
- * 
- * @deprecated In practice, the classes in this package have very, very few use cases and their maintenance cost
- * is not justified.
- */
-package space.arim.api.util.collect;
