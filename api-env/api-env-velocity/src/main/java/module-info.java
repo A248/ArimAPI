b643d3601925e1720c3222645e0fc03dde14cbe5
A248 <theanandbeh@gmail.com>
2022-07-22 16:10:15 -0400

    Properly export space.arim.api.env.velocity

--- api-env/api-env-velocity/src/main/java/module-info.java
+++ api-env/api-env-velocity/src/main/java/module-info.java
@@ -2,4 +2,5 @@ module space.arim.api.env.velocity {
 	requires transitive com.velocitypowered.api;
 	requires transitive space.arim.api.env;
 	requires space.arim.omnibus;
+	exports space.arim.api.env.velocity;
 }
