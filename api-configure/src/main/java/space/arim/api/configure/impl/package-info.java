876f669e5edb615dd987612cd97f723ccdfe1e5c
A248 <a248-dev@tuta.io>
2026-07-04 09:07:24 -0700

    Delete long-deprecated module

--- api-configure/src/main/java/space/arim/api/configure/impl/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Helpful base classes for implementers of configuration interfaces.
- *
- * @deprecated See deprecation of {@link space.arim.api.configure} (this entire framework is deprecated)
- */
-package space.arim.api.configure.impl;
--- api-configure/src/main/java/space/arim/api/configure/yaml/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes related specifically to the YAML based implementation of {@link ConfigSerialiser}.
- *
- * @deprecated See deprecation of {@link space.arim.api.configure} (this entire framework is deprecated)
- */
-package space.arim.api.configure.yaml;
-
-import space.arim.api.configure.ConfigSerialiser;
